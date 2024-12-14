ff46da8f0b6b827e0b4fc04b11adc8a6954cc152
Anshuman Ranjan <anshumanranjan1998@gmail.com>
2024-12-14 10:12:41 +0000

    1. Class TwoFactorAuth fixed 2. @OneToOne added to TwoFactorAuth in User entity. 3. Added docker-compose file.

--- src/main/java/com/trading/trading/modal/TwoFactorAuth.java
+++ src/main/java/com/trading/trading/modal/TwoFactorAuth.java
@@ -2,9 +2,15 @@ package com.trading.trading.modal;
 
 import com.trading.trading.domain.VerificationType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class TwoFactorAuth {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private boolean isEnabled = false;
     private VerificationType sendTo;
 }
--- src/main/java/com/trading/trading/modal/User.java
+++ src/main/java/com/trading/trading/modal/User.java
@@ -3,9 +3,7 @@ package com.trading.trading.modal;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.trading.trading.domain.USER_ROLE;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Entity
 @Data
@@ -19,7 +17,7 @@ public class User {
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
 
-    @Embedded
+    @OneToOne
     private TwoFactorAuth twoFactorAuth = new TwoFactorAuth();
 
     private USER_ROLE role = USER_ROLE.ROLE_CUSTOMER;
